fff52c79da72cbc4a5a68229791b88be84623dc9
hyraxhomie <will.suchanek@gmail.com>
2025-09-13 15:40:24 -0400

    Count texts

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def count_words(text):
+    return len(text.split())
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(text)
+    print(f"{num_words} words found in the document")
+
+main()
